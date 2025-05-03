cdd7bc11ecfc3e85e934e090824080302765006a
BreannaBre <breanna.may.breedlove@gmail.com>
2025-05-03 17:01:42 -0500

    fully implemented, but may add user preferences

--- write_frequency.py
+++ write_frequency.py
@@ -1,8 +1,16 @@
 #!/bin/python3
-#this should print every time write is called
 from bcc import BPF
 from bcc.utils import printb
 import os
+import sys
+import time
+
+# CHANGE THESE PARAMETERS to set when a process should be flagged
+# write_limit is the max amount of writes a process can have in a set amount of time
+write_limit = 100
+# time_limit is the length of time the writes must occur in for it to be flagged
+# ex: if 100 writes occur within 2 seconds, flag the program
+time_limit_seconds = 2
 
 
 # BPF program
@@ -16,8 +24,9 @@ struct data_t {
     char comm[TASK_COMM_LEN];
     u64 timestamp;
 };
+
 //pass event info though events
-BPF_PERF_OUTPUT(events);
+BPF_RINGBUF_OUTPUT(events, 8);
 
 int write_func(struct pt_regs *ctx)
 {
@@ -27,7 +36,7 @@ int write_func(struct pt_regs *ctx)
     bpf_get_current_comm(&data.comm, sizeof(data.comm));
     data.timestamp = bpf_ktime_get_ns();
 
-    events.perf_submit(ctx, &data, sizeof(data));
+    events.ringbuf_output(&data, sizeof(data), 0);
 
     return 0;
 }
@@ -37,45 +46,57 @@ int write_func(struct pt_regs *ctx)
 b = BPF(text=program)
 b.attach_kprobe(event="vfs_write", fn_name="write_func")
 
-print("Tracing sys_sync()... Ctrl-C to end")
+print("Tracing write_sync()... Ctrl-C to end")
 
-# dictionary keeps track of when processes have written
+# keep track of timpstamps when processes wrote, additional info of process (such as name), and a count of times its been flagged
 process_ts_list = {}
 process_info = []
+offending_processes = {}
 
-write_limit = 100
-time_limit = 2000000000
+# function to check and flag after inital removal/appending has occured for process
+def check_flagging(pid, comm):
+    if len(process_ts_list[pid]) > write_limit:
+        if pid not in offending_processes:
+            offending_processes[pid] = 1
+        else:
+            offending_processes[pid] += 1
+        # reset list and log if it occurs again
+        process_ts_list[pid] = []
+        print("Offending process pid: " + str(pid) + " name: " + str(comm))
 
-def print_event(cpu, data, size):
+# function that handles data when a write occurs
+def document_event(cpu, data, size):
     event = b["events"].event(data)
     # check if process has already written previously
     if event.pid not in process_ts_list:
         process_ts_list[event.pid] = []
         process_ts_list[event.pid].append(event.timestamp)
-        process_info.append((event.pid, str(event.comm)))
+        process_info.append((event.pid, str(event.comm.decode())))
     else:
     # check oldest timestamp(s) and remove after a fixed parameter
         all_olds_removed = False
         while not all_olds_removed:
-            if event.timestamp - process_ts_list[event.pid][0] >  time_limit:
-                process_ts_list[event.pid].pop(0)
-                if len(process_ts_list[event.pid]) == 0:
+            if len(process_ts_list[event.pid]) == 0:
                     all_olds_removed = True
+            elif event.timestamp - process_ts_list[event.pid][0] >  (time_limit_seconds * 1000000000):
+                process_ts_list[event.pid].pop(0)
             else:
                 all_olds_removed = True
         process_ts_list[event.pid].append(event.timestamp)
-    # check if the limit to cause a flag is reached:
+    check_flagging(event.pid, event.comm.decode())
+
     return 0
 
-# output
-b["events"].open_perf_buffer(print_event)
-while 1:
+# This is the *main* part of the program
+b["events"].open_ring_buffer(document_event)
+while True:
     try:
-        b.perf_buffer_poll()
-    except ValueError:
-        continue
+        b.ring_buffer_poll()
+        time.sleep(0.5)
     except KeyboardInterrupt:
+        print("\nSummary of Flagged Processes:")
+        printb(b"%-8s %-20s %-5s" % (b"PID", b"NAME", b"NUM TIMES FLAGGGED"))
         for p in process_info:
-            # TODO: format output better
-            print("pid: " + str(p[0]) + " name: " + p[1] + " num of processes: " + str(len(process_ts_list[p[0]])))
-        exit()
+            if p[0] in offending_processes:
+                print("%-8d %-20s %-5d" % (p[0], p[1], offending_processes[p[0]]))
+        sys.exit()
